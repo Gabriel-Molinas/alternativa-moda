4c0826de3bf7decac6407973b7505a322f6d21df
Gabriel-Molinas <gabubicho@gmail.com>
2021-04-30 17:55:23 -0300

    redireccion | PD:Alto dia

---
+++ public/js/redirect.js
@@ -0,0 +1,36 @@
+const exicalDigital = document.getElementById("exicalDigital"),
+  micam = document.getElementById("micam"),
+  futurmoda = document.getElementById("futurmoda"),
+  sicc = document.getElementById("sicc"),
+  aplf = document.getElementById("aplf"),
+  fashionAccess = document.getElementById("fashionAccess"),
+  leatherShoetech = document.getElementById("leatherShoetech"),
+  zero = document.getElementById("zero");
+
+exicalDigital.addEventListener("click", () => {
+  window.open("https://exical.com.ar/", "_blank");
+});
+micam.addEventListener("click", () => {
+  window.open("https://themicam.com/", "_blank");
+});
+futurmoda.addEventListener("click", () => {
+  window.open("https://www.futurmoda.es/", "_blank");
+});
+sicc.addEventListener("click", () => {
+  window.open(
+    "https://www.sicc.com.br/noticia/2154/pr-ximo-sicc-presencial-acontece-em-2021",
+    "_blank"
+  );
+});
+aplf.addEventListener("click", () => {
+  window.open("https://www.aplf.com/#1", "_blank");
+});
+fashionAccess.addEventListener("click", () => {
+  window.open("https://www.fashionaccess.aplf.com/en-us/", "_blank");
+});
+leatherShoetech.addEventListener("click", () => {
+  window.open("https://www.chinaleatherfair.com/", "_blank");
+});
+zero.addEventListener("click", () => {
+  window.open("https://www.feirazerograu.com.br/", "_blank");
+});
