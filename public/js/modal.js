c88b1da53602e8bc417450d5fc412f352da257f0
Gabriel-Molinas <gabubicho@gmail.com>
2021-05-12 11:05:03 -0300

    Shuter

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,6 +1,3 @@
-var myModal = document.getElementById("myModal");
-var myInput = document.getElementById("myInput");
-
-myModal.addEventListener("shown.bs.modal", function () {
-  myInput.focus();
+var myModal = new bootstrap.Modal(document.getElementById("loginButton"), {
+  backdrop: false,
 });
